818fa335448bd4aab48a3274c2f339903adaf536
Ivan <ivanferrera67@gmail.com>
2022-03-01 16:09:38 +0100

    He hecho la iteracion 1

---
+++ iteration1.js
@@ -0,0 +1,5 @@
+function sum(numberOne , numberTwo) {
+    ( numberOne > numberTwo) ? console.log(numberOne + " es mayor que " + numberTwo) : console.log(numberTwo + " es mayor que " + numberOne);
+  }
+
+  sum(7,9)
